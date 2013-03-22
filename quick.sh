9d6ecd0b5cd49989e9ec714f36dd18216571d1be
khayer <katharinaehayer@gmail.com>
2013-03-22 15:44:08 -0400

    hitting the pause button

--- quick.sh
+++ quick.sh
@@ -1,4 +1,4 @@
-for i in {1..20}
+for i in {9..20}
 do
   python zero_maze ../Downloads/Material\ for\ Tracking/mouse$i.m4v mouse_foo.png > /Users/kat/Desktop/results_anand/mouse$i.txt
 done
--- quick2.sh
+++ quick2.sh
@@ -1,4 +1,4 @@
-for i in 557 539 231 #227 230 231 538 539 541 544 546 547 548 549 550 553 556 557 558 561 562 070811-543-b
+for i in 225 #557 539 231 #227 230 231 538 539 541 544 546 547 548 549 550 553 556 557 558 561 562 070811-543-b
 do
   python zero_maze ~/Downloads/Material\ for\ Tracking/$i.m4v ~/Downloads/Material\ for\ Tracking/foo.png > /Users/kat/Desktop/results_anand/$i.txt
 done
