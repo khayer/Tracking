f5fb0ed63e6f59c03fb1cd372e88b9c165afd360
khayer <katharinaehayer@gmail.com>
2013-03-22 15:56:28 -0400

    hitting the pause button

--- fast_tracking/fast_tracking.py
+++ fast_tracking/fast_tracking.py
@@ -26,7 +26,6 @@ class Target:
     def get_background(self,video):
         capture = cv2.VideoCapture(video)
         _,frame = capture.read()
-        
         average = float32(frame)
         print >> stderr, "background start"
         for i in range(1,self.length_cali):
@@ -40,11 +39,9 @@ class Target:
             if l%2==0:
                stderr.write("\r[%-50s] %d%%" % ('='*int(l), percent))
                stderr.flush()
-
         stderr.write("\r[%-50s] %d%%\n" % ('='*50, 100))
         print >> stderr, "background done"
         return cv.fromarray(background)
-
 
     def get_open_arm(self):
         ######## Define open arms area
@@ -54,17 +51,10 @@ class Target:
         hsv = cv.CreateImage(self.size, 8, 3)
         sat = cv.CreateImage(self.size, 8, 1)
         black_mouse = cv.CreateImage(self.size, 8, 1)
-        #split image into hsv, grab the sat
         cv.CvtColor(self.image, hsv, cv.CV_BGR2HSV)
-        #cv.ShowImage("HAHA", hsv)
         cv.Split(hsv, None, sat, None, None)
-        #cv.ShowImage("HIHI", sat)
-        #split image into rgb
         cv.Split(self.image, None, None, red, None)
-        #cv.ShowImage("HEHE", red)
-        #find the car by looking for red, with high saturation
         cv.Threshold(red, red, 128, 255, cv.CV_THRESH_BINARY)
-        #cv.ShowImage("HOHO", red)
         cv.Threshold(sat, sat, 128, 255, cv.CV_THRESH_BINARY)
         cv.Mul(red, sat, open_arms)
         return open_arms
@@ -80,30 +70,21 @@ class Target:
         cv.Split(hsv, None, sat, None, None)
         #split image into rgb
         cv.Split(self.image, blue, None, None, None)
-        #find the car by looking for red, with high saturation
         cv.Threshold(blue, blue, 128, 255, cv.CV_THRESH_BINARY)
         cv.Threshold(sat, sat, 128, 255, cv.CV_THRESH_BINARY)
-        #AND the two thresholds, finding the car
         cv.Mul(blue, sat, closed_arm)
         return closed_arm
 
     def get_zeromaze(self):
         mix_open_closed = cv.CreateImage(self.size, 8, 1)
         cv.Or(self.open_arm, self.closed_arm, mix_open_closed)
-        #cv.SaveImage('test_close.png',self.closed_arm)
-        #cv.SaveImage('test_open.png',self.open_arm)
         cv.Dilate(mix_open_closed, mix_open_closed, None, 10)
-        #cv.SaveImage('test_eroded.png',mix_open_closed)
         cv.Erode(mix_open_closed, mix_open_closed, None, 10)
-        #cv.SaveImage('test_mix.png',mix_open_closed)
         return mix_open_closed
-
-
 
     def distance_func(self, points):
         p0, p1 = points
         return (p0[0] - p1[0])**2 + (p0[1] - p1[1])**2
-
 
     def where_is_the_nose(self,max_pair, image ):
         white_space = 0
@@ -119,145 +100,78 @@ class Target:
             cv.ResetImageROI(image)
         return nose_coord
 
-    #def distance(points):
-     #   p0, p1 = points
-      #  return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
-
-    ###########################
-
     def get_mouse_area(self):
-        """ """
-
         print >> stderr, "Calibration ..."
         frame = cv.QueryFrame(self.capture)
         frame_size = cv.GetSize(frame)
-
         # Jump 30 second into video file
         cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC, 30000)
-
         color_image = cv.CreateImage(frame_size, 8, 3)
         grey_image = cv.CreateImage(frame_size, cv.IPL_DEPTH_8U, 1)
         moving_average = cv.CreateImage(frame_size, cv.IPL_DEPTH_32F, 3)
         black_mouse = cv.CreateImage(frame_size, 8, 1)
-
         first = True
         areas = zeros(self.length_cali)
-
         for i in range(1,self.length_cali):
             grey_image = cv.CreateImage(frame_size, cv.IPL_DEPTH_8U, 1)
-
             color_image = cv.QueryFrame(self.capture)
-            #print frame_size
-            #cv.SaveImage('test_png_mouse.png',color_image)
-            #break
-            #k = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC)
-            #print k
             cv.InRangeS(color_image,cv.Scalar(0,0,0),cv.Scalar(4,4,4),black_mouse) # Select a range of blue color
             cv.ShowImage("Mouse Color 1",black_mouse)
             cv.And(black_mouse,self.zero_maze,black_mouse)
             cv.ShowImage("Mouse Color 2",black_mouse)
-
             cv.Erode(black_mouse, black_mouse, None, 3)
             cv.Dilate(black_mouse, black_mouse, None, 8)
             cv.Erode(black_mouse, black_mouse, None, 3)
-
-            #cv.ShowImage("Mouse Color Rendered", black_mouse)
-
-            #cv.InRangeS(color_image,cv.Scalar(0,0,0),cv.Scalar(4,4,4),black_mouse) # Select a range of blue color
-            #cv.Erode(black_mouse, black_mouse, None, 4)
-            #cv.Dilate(black_mouse, black_mouse, None, 10)
             if first:
                 difference = cv.CloneImage(color_image)
-            #    temp = cv.CloneImage(color_image)
-            #    cv.ConvertScale(color_image, moving_average, 1.0, 0.0)
                 first = False
-            #else:
-            #    cv.RunningAvg(color_image, moving_average, 0.0005, None)
-
-            #temp1 = cv.CloneImage(color_image)
-            #cv.ShowImage("temp1", temp)
-            #cv.ShowImage("moving_average", moving_average)
-            # Smooth to get rid of false positives
-
             cv.Smooth(color_image, color_image, cv.CV_GAUSSIAN, 7, 7)
-            #cv.RunningAvg(color_image, moving_average, 0.0001, None)
-            # Convert the scale of the moving average.
-            #cv.ConvertScale(moving_average, temp, 1.0, 0.0)
-            #cv.ShowImage("temp", temp)
-            # Minus the current frame from the moving average.
             cv.AbsDiff(color_image, self.background, difference)
             cv.ShowImage("difference", difference)
-            #cv.ShowImage("color_image", color_image)
-            #cv.ShowImage("temp", temp)
-
             # Convert the image to grayscale.
             cv.CvtColor(difference, grey_image, cv.CV_RGB2GRAY)
             cv.ShowImage("Only Movement 1", grey_image)
             ### Where do both pictures overlap
-            #cv.And(grey_image, black_mouse, grey_image)
             # Convert the image to black and white.
             cv.Threshold(grey_image, grey_image, 75, 255, cv.CV_THRESH_BINARY)
             cv.Erode(grey_image, grey_image, None, 1)
-            #cv.Dilate(grey_image, grey_image, None, 4)
             cv.ShowImage("Only Movement", grey_image)
-            
-            # Dilate and erode to get people blobs
+            # Dilate and erode to get mouse shapes
             cv.And(grey_image, black_mouse, grey_image)
             cv.ShowImage("Mix",grey_image)
-            
-            #cv.ShowImage("LALA", grey_image)
-            #cv.ShowImage("LALe", color_image)
             storage = cv.CreateMemStorage(0)
             contour = cv.FindContours(grey_image, storage, cv.CV_RETR_CCOMP, cv.CV_CHAIN_APPROX_SIMPLE)
             if contour:
                 areas[i] = cv.ContourArea(contour)
-
-
-
             c = cv.WaitKey(7) % 0x100
-
-
             cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC, i * 100)
-
             percent = i/(self.length_cali/100)
             l = int(percent/2)
             if l%2==0:
                stderr.write("\r[%-50s] %d%%" % ('='*int(l), percent))
                stderr.flush()
-
         areas = sort(areas)
         last_position_zeros = max(where(areas == 0)[0])
         areas = areas[self.length_cali/2:self.length_cali]
-        #print mean(areas)
-        #print median(areas)
-        #print std(areas)
         stderr.write("\r[%-50s] %d%%\n" % ('='*50, 100))
         print >> stderr, "Preparing done"
         c = cv.WaitKey(7) % 0x100
         cv.DestroyWindow("Mix")
-
-        #print areas
         print >> stderr, mean(areas)
-        #return [mean(areas)-3*std(areas), mean(areas)+std(areas)]
         return [mean(areas)/5,mean(areas)+std(areas)]
 
     def run(self):
-
-
-
         frame = cv.QueryFrame(self.capture)
         cv.ShowImage("Original",frame)
         k = cv.SetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC, 0)
         frame_size = cv.GetSize(frame)
         start_total = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC)
-
         # Preparing files
         color_image = cv.CreateImage(frame_size, 8, 3)
         grey_image = cv.CreateImage(frame_size, cv.IPL_DEPTH_8U, 1)
         moving_average = cv.CreateImage(frame_size, cv.IPL_DEPTH_32F, 3)
         tmp_image_open_arms = cv.CreateImage(frame_size, 8, 1)
         tmp_image_closed_arms = cv.CreateImage(frame_size, 8, 1)
-
         time_on_open_arm = 0
         start = 0
         end = 0
@@ -272,10 +186,8 @@ class Target:
         start_bouts_open_arm = 400
         end_bouts_open_arm = 0
         time_bout_open_arm = 0
-
         first = True
         is_open_arm = False
-
         distance = 0
         point1 = (0,0)
         point2 = (0,0)
@@ -284,15 +196,12 @@ class Target:
         point5 = (0,0)
         center_point_old = (0,0)
         center_point = (0,0)
-
         cv.Smooth(self.image, self.image, cv.CV_GAUSSIAN, 1, 0)
         open_arms = cv.CreateImage(self.size, 8, 1)
         red = cv.CreateImage(self.size, 8, 1)
         hsv = cv.CreateImage(self.size, 8, 3)
         sat = cv.CreateImage(self.size, 8, 1)
         black_mouse = cv.CreateImage(self.size, 8, 1)
-       
-
         frame_number = 0
         percent_in_video = 0
         counter_for_bouts = 0
@@ -306,7 +215,6 @@ class Target:
             frame_number += 2
 
             cv.InRangeS(color_image,cv.Scalar(0,0,0),cv.Scalar(4,4,4),black_mouse) # Select a range of blue color
-            #cv.Erode(black_mouse, black_mouse, None, 1)
             cv.And(black_mouse,self.zero_maze,black_mouse)
             cv.Erode(black_mouse, black_mouse, None, 1)
             cv.Dilate(black_mouse, black_mouse, None, 7)
@@ -435,12 +343,6 @@ class Target:
                 point3 = point4
                 point4 = point5
 
-            # Problems to solve:
-            # 1) Time spent on the open arm
-            # 2) No. of transitions from closed arm
-            # 3) Speed of the mouse in the open arm
-            # 4) Total distance traveled
-
             percent = int(percent_in_video * 100)
             l = int(percent_in_video * 100 / 2)
             if l%2==0:
@@ -485,7 +387,6 @@ class Target:
 
         end_total = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC)
         total_time = (end_total-start_total) / 1000
-
         stderr.write("\r[%-50s] %d%%\n" % ('='*50, 100))
         print "total distance traveled in pixel\t", distance/self.conversion
         print "total length of experiment\t", total_time
@@ -494,8 +395,6 @@ class Target:
         print "distance on closed arm\t", (distance - distance_open_arm)/self.conversion
         print "s spent on closed arm\t", total_time - time_on_open_arm
         print "speed in pixel per second\t", ((distance - distance_open_arm)/self.conversion) / (total_time - time_on_open_arm)
-        #print distance / 108.78 , " inches"
-        #print (distance / 108.78) * 2.54 , " cm"
         print "--- Open arm ----------------------------------------"
         print "distance on open arm\t", distance_open_arm/self.conversion
         print "s spent on open arm\t", time_on_open_arm
@@ -507,16 +406,6 @@ class Target:
         print "transitions from closed->open\t", number_of_transitions_closed_open
         print "transitions from open->closed\t", number_of_transitions_open_closed
         print "--- Bouts ----------------------------------------"
-#  for zero maze - we can threshold time at 0.25sec. So any contiguous movement for longer than 0.25 sec will be recorded separately - these are called bouts/ambulatory movements. So for each bout recorded we can have the typical parameters like
-# (a) in open/closed arm
-# (b) average speed during that bout
         print "Bouts in s in open arm\t", time_bout
         print "Speed in open arm\t", (distance_bout / self.conversion)/time_bout
-        #print "Bouts in s in open arm\t", time_bout_open_arm
-        #print "Speed in open arm\t", (distance_bout_open_arm/self.conversion)/time_bout_open_arm
-        #time_bout_closed_arm = time_bout - time_bout_open_arm
-        #distance_bout_closed_arm = distance_bout - distance_bout_open_arm
-        #print "Bouts in s in closed arm\t", time_bout_closed_arm
-        #print "Speed in closed arm\t", (distance_bout_closed_arm/self.conversion)/time_bout_closed_arm
-# (c) number of such bouts
         print "# of bouts\t", number_of_bouts
