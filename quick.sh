4e4e80df0f5ee13e09ce635619ccb1ca1e0b2030
Katharina Hayer <katharinaehayer@gmail.com>
2013-06-27 15:51:05 -0400

    hitting the pause button

--- quick.sh
+++ quick.sh
@@ -1,4 +1,4 @@
-for i in {9..20}
+for i in {1..20}
 do
-  python zero_maze ../Downloads/Material\ for\ Tracking/mouse$i.m4v mouse_foo.png > /Users/kat/Desktop/results_anand/mouse$i.txt
+  python zero_maze ../Downloads/Material\ for\ Tracking/mouse$i.m4v mouse_foo.png > /Users/kat/Desktop/results_anand/mouse$i\_new.txt
 done
